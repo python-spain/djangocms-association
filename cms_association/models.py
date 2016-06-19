40125c5dad2f70ee49dfbc99790f00686b45fdd2
Nekmo <contacto@nekmo.com>
2016-06-19 05:14:02 +0200

    Social data and person models.

--- cms_association/models.py
+++ cms_association/models.py
@@ -1,11 +1,62 @@
-from cities.models import District, City, Subregion, Region, Country
+from cities.models import District, City, Subregion, Region
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.contrib.gis.db.models import PointField
 from django.core.exceptions import ValidationError
+from django.core.validators import URLValidator, EmailValidator
 from django.utils.translation import ugettext_lazy as _
 from django.db import models
+from phonenumber_field.validators import validate_international_phonenumber
+
+
+SOCIAL_TYPES = [
+    (
+        _('VCS'),
+        (
+            ('GITHUB', _('Github')),
+            ('BITBUCKET', _('Bitbucket')),
+            ('GITLAB', _('Gitlab')),
+        ),
+    ),
+    (
+        _('Social'),
+        (
+            ('TWITTER', _('Twitter')),
+            ('FACEBOOK', _('Facebook')),
+            ('GOOGLE-PLUS', _('Google+')),
+            ('LINKEDIN', _('LinkedIn')),
+            ('PINTEREST', _('Pinterest')),
+            ('TUMBLR', _('Tumblr')),
+            ('WEBSITE', _('Website'), {'validator': URLValidator}),
+        ),
+    ),
+    (
+        _('Communication'),
+        (
+            ('TELEPHONE', 'Telephone', {'validator': validate_international_phonenumber}),
+            ('MOBILE', 'Mobile', {'validator': validate_international_phonenumber}),
+            ('WHATSAPP', 'WhatsApp', {'validator': validate_international_phonenumber}),
+            ('TELEGRAM', 'Telegram'),
+            ('SKYPE', 'Skype'),
+            ('FMESSENGER', 'Facebook Messenger'),
+            ('LINE', 'Line'),
+            ('IRC', 'IRC'),
+            ('JABBER', 'Jabber/XMPP', {'validator': EmailValidator}),
+        ),
+    ),
+]
+
+
+def remove_social_options(sections):
+    """Remove additional options in SOCIAL_TYPES
+    """
+    for section in sections:
+        yield section[0], [choice[:2] for choice in section[1]]
 
 
 class AbstractAddress(models.Model):
+    """
+    """
     street = models.CharField(max_length=120)
     poiny = PointField(blank=True, null=True)
     district = models.ForeignKey(District, blank=True, null=True, on_delete=models.SET_NULL)
@@ -25,6 +76,22 @@ class AbstractAddress(models.Model):
 
     class Meta:
         abstract = True
+
+
+class AbstractSocialMedia(models.Model):
+    """A model to have social media key-value pairs. For example, twitter, Facebook, etc.
+    """
+    type = models.CharField(choices=remove_social_options(SOCIAL_TYPES))
+    value = models.CharField()
+
+    class Meta:
+        abstract = True
+
+
+class GenericSocialMedia(AbstractSocialMedia):
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
 
 
 class Address(AbstractAddress):
--- cms_people/models.py
+++ cms_people/models.py
@@ -2,8 +2,13 @@ from django.conf import settings
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
+from cms_association.models import Address
+from taggit.managers import TaggableManager
+
 
 class Person(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, verbose_name=_('User'))
     bio = models.TextField(_('Bio'), blank=True)
-    avatar = models.ImageField()
+    avatar = models.ImageField(upload_to='avatars', blank=True, null=True)
+    address = models.OneToOneField(Address, blank=True, null=True, on_delete=models.SET_NULL)
+    interests = TaggableManager()
