0aa4fd7b31c024f3cec933ca57405751126d4455
Nekmo <contacto@nekmo.com>
2016-06-19 06:51:44 +0200

    Event dates, urls and target.

--- cms_event/models.py
+++ cms_event/models.py
@@ -12,16 +12,28 @@ BOOKING_OPTIONS = [
 ]
 
 
+EVENT_TARGET = [
+    ('LOCAL', _('Local')),
+    ('REGION', _('Region')),
+    ('COUNTRY', _('Country')),
+    ('INTERNATIONAL', _('International')),
+]
+
+
 class Event(AbstractContact):
     name = models.CharField(max_length=60, label=_('Name'))
     edition = models.PositiveSmallIntegerField(default=1, verbose_name=_('Edition'))
     slug = models.SlugField()
+    target = models.CharField(max_length=18, choices=EVENT_TARGET, default='LOCAL')
     description = models.TextField(blank=True)
     association = models.ForeignKey(Association, on_delete=models.CASCADE)
-    start_datetime = models.DateTimeField()
-    end_datetime = models.DateTimeField()
+    start_datetime = models.DateTimeField(blank=True, null=True)
+    end_datetime = models.DateTimeField(blank=True, null=True)
     booking = models.CharField(choices=BOOKING_OPTIONS, default='OPTIONAL')
     total_seats = models.PositiveSmallIntegerField(blank=True, null=True)
+    website_url = models.URLField(blank=True, null=True)
+    booking_url = models.URLField(blank=True, null=True)
+    schedule_url = models.URLField(blank=True, null=True)
 
 
 class PriceEvent(models.Model):
@@ -29,3 +41,4 @@ class PriceEvent(models.Model):
     name = models.CharField(max_length=30, label=_('Name'))
     price = models.DecimalField()
     description = models.CharField(max_length=150, blank=True)
+    seats = models.PositiveSmallIntegerField(blank=True, null=True)
