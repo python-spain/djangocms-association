4bf245773116045d44d30498871802fa95ba7139
Nekmo <contacto@nekmo.com>
2016-06-19 07:10:31 +0200

    Schedule.

--- cms_event/models.py
+++ cms_event/models.py
@@ -1,14 +1,22 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+from taggit.managers import TaggableManager
 
 from cms_association.models import Association
 from cms_contact.models import AbstractContact
-
+from cms_people.models import Person
 
 BOOKING_OPTIONS = [
     ('OPTIONAL', _('Available')),
     ('REQUIRED', _('Required')),
     ('UNAVAILABLE', _('Free entry'))
+]
+
+
+ATTENDANCE_OPTIONS = [
+    ('YES', _('Yes')),
+    ('NO', _('No')),
+    ('MAYBE', _('Maybe')),
 ]
 
 
@@ -26,7 +34,7 @@ class Event(AbstractContact):
     slug = models.SlugField()
     target = models.CharField(max_length=18, choices=EVENT_TARGET, default='LOCAL')
     description = models.TextField(blank=True)
-    association = models.ForeignKey(Association, on_delete=models.CASCADE)
+    association = models.ForeignKey(Association, on_delete=models.CASCADE, blank=True, null=True)
     start_datetime = models.DateTimeField(blank=True, null=True)
     end_datetime = models.DateTimeField(blank=True, null=True)
     booking = models.CharField(choices=BOOKING_OPTIONS, default='OPTIONAL')
@@ -34,6 +42,7 @@ class Event(AbstractContact):
     website_url = models.URLField(blank=True, null=True)
     booking_url = models.URLField(blank=True, null=True)
     schedule_url = models.URLField(blank=True, null=True)
+    attendances = models.ManyToManyField(Person, through='Attendance')
 
 
 class PriceEvent(models.Model):
@@ -42,3 +51,26 @@ class PriceEvent(models.Model):
     price = models.DecimalField()
     description = models.CharField(max_length=150, blank=True)
     seats = models.PositiveSmallIntegerField(blank=True, null=True)
+
+
+class Attendance(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    attendance = models.CharField(max_length=12, choices=ATTENDANCE_OPTIONS)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Schedule(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    name = models.CharField(max_length=80)
+    description = models.TextField(blank=True)
+    interests = TaggableManager(label=_('Interests'))
+    speakers = models.ManyToManyField(Person)
+    custom_speakers = models.CharField(max_length=150, blank=True)
+    start_datetime = models.DateTimeField(blank=True, null=True)
+    duration = models.DurationField(blank=True, null=True)
+    location = models.CharField(blank=True, max_length=80, label=_('Location'),
+                                help_text=_('Location within the building or buildings.'))
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
