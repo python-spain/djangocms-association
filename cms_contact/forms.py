da50dac0021136b8a5d16ca31915737898bb405c
Nekmo <contacto@nekmo.com>
2016-06-26 21:02:29 +0200

    Select2 filter for cities.

--- cms_contact/forms.py
+++ cms_contact/forms.py
@@ -7,9 +7,19 @@ from cms_contact.models import Address
 
 class CityWidget(ModelSelect2Widget):
     queryset = City.objects.all()
+    width = '300px'
     search_fields = [
         'name__icontains'
     ]
+
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault('attrs', {'style': 'width: {};'.format(self.width)})
+        super(CityWidget, self).__init__(*args, **kwargs)
+
+    def build_attrs(self, extra_attrs=None, **kwargs):
+        extra_attrs = extra_attrs or {}
+        extra_attrs.setdefault('theme', 'bootstrap')
+        return super(CityWidget, self).build_attrs(extra_attrs, **kwargs)
 
 
 class AddressForm(ModelForm):
