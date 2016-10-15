4e960f0a1113689592693b7b534bcdc356f6b459
Nekmo <contacto@nekmo.com>
2016-10-15 23:24:49 +0200

    Profile urls fixed and dynamic profile menu.

--- cms_people/urls/profile.py
+++ cms_people/urls/profile.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 
-from cms_people.views import ProfileAboutView
+from cms_people.views import ProfileAboutView, ProfileSecurityView
 from ..views import ProfileAddressView
 
 urlpatterns = [
     url('^$', ProfileAboutView.as_view(), name='profile_about'),
+    url('^security$', ProfileSecurityView.as_view(), name='profile_security'),
     url('^address$', ProfileAddressView.as_view(), name='profile_address'),
 ]
--- cms_people/views.py
+++ cms_people/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.core.urlresolvers import reverse
 from django.http import HttpResponseForbidden
 from django.views.generic import UpdateView
 
@@ -6,8 +7,25 @@ from cms_contact.forms import AddressForm
 from cms_contact.models import Address
 from cms_people.models import Person
 
+from django.utils.translation import ugettext as _
+
+
+TABS = [
+    {'tab': 'about', 'name': _('About'), 'url_name': 'profile_about'},
+    {'tab': 'security', 'name': _('Email and password'), 'url_name': 'profile_security'},
+    {'tab': 'address', 'name': _('Address'), 'url_name': 'profile_address'},
+]
+
 
 class ProfileView(UpdateView):
+    profile_tab = ''
+
+    def get_context_data(self, **kwargs):
+        context = super(ProfileView, self).get_context_data(**kwargs)
+        context['profile_tab'] = self.profile_tab
+        context['tabs'] = TABS
+        return context
+
     def dispatch(self, request, *args, **kwargs):
         if not request.user.is_authenticated():
             return HttpResponseForbidden()
@@ -15,6 +33,7 @@ class ProfileView(UpdateView):
 
 
 class ProfileAboutView(ProfileView):
+    profile_tab = 'about'
     model = Person
     template_name = 'cms_people/about.html'
     fields = ('bio',)
@@ -25,14 +44,17 @@ class ProfileAboutView(ProfileView):
 
 
 class ProfileSecurityView(ProfileView):
+    profile_tab = 'security'
     model = get_user_model()
     template_name = 'cms_people/security.html'
+    fields = ('email', 'password')
 
     def get_object(self, queryset=None):
         return self.request.user
 
 
 class ProfileAddressView(ProfileView):
+    profile_tab = 'address'
     model = Address
     form_class = AddressForm
     template_name = 'cms_people/address.html'
