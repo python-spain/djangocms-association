4a7b01c1d3702dcf618fe5d64cf40aa96fcd84fc
Nekmo <contacto@nekmo.com>
2016-10-28 00:25:35 +0200

    Changed avatar upload_to and auto-remove old avatar on save.

--- cms_people/models.py
+++ cms_people/models.py
@@ -25,13 +25,18 @@ EMAIL_PRIVACY = REAL_NAME_PRIVACY
 TELEPHONES_PRIVACY = REAL_NAME_PRIVACY
 
 
+def avatar_path(instance, filename):
+    ext = filename.split('.')[-1]
+    return 'avatars/{}.{}'.format(instance.user.id, ext)
+
+
 class Person(AbstractContact):
     """A person corresponds to a Django user.
     It includes additional fields and privacy policies.
     """
     user = models.OneToOneField(settings.AUTH_USER_MODEL, verbose_name=_('User'), related_name='person')
     bio = models.TextField(verbose_name=_('Biography'), blank=True, max_length=12000)
-    avatar = models.ImageField(verbose_name=_('Avatar'), upload_to='avatars', blank=True, null=True)
+    avatar = models.ImageField(verbose_name=_('Avatar'), upload_to=avatar_path, blank=True, null=True)
     # privacy policies
     real_name_privacy = models.CharField(max_length=20, verbose_name=_('Real name privacy'), choices=REAL_NAME_PRIVACY,
                                          default='VISIBLE')
@@ -49,3 +54,10 @@ class Person(AbstractContact):
     nearby_new_associations = models.BooleanField(default=True, verbose_name=_('Nearby new associations'))
     nearby_new_jobs = models.BooleanField(default=False, verbose_name=_('Nearby new jobs'))
     nearby_new_events = models.BooleanField(default=True, verbose_name=_('Nearby new events'))
+
+    def save(self, **kwargs):
+        if self.pk:
+            person = Person.objects.get(pk=self.pk)
+            if self.avatar != person.avatar:
+                person.avatar.delete(save=False)
+        super(Person, self).save(**kwargs)
