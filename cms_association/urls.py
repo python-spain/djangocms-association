5a1c69da123ad8619995e171f2db39d8c7155e8d
Nekmo <contacto@nekmo.com>
2016-11-03 03:56:11 +0100

    Association view.

--- cms_association/urls.py
+++ cms_association/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
 
-from cms_association.views import AssociationsView, AssociationMapResultView
+from cms_association.views import AssociationsView, AssociationMapResultView, AssociationView
 
 urlpatterns = [
     url('^people_map_result$', AssociationMapResultView.as_view(), name='association_map_result'),
-    # url('^(?P<association>.+)$', AssociationMapResultView.as_view(), name='association'),
+    url('^(?P<association>.+)$', AssociationView.as_view(), name='association'),
     url('^$', AssociationsView.as_view(), name='associations'),
 ]
--- cms_association/views.py
+++ cms_association/views.py
@@ -1,3 +1,6 @@
+from django.shortcuts import get_object_or_404
+from django.views.generic import DetailView
+
 from cms_association.models import Association
 from cms_contact.views.map import MapView, MapResultView
 
@@ -10,3 +13,15 @@ class AssociationsView(MapView):
 class AssociationMapResultView(MapResultView):
     queryset = Association.objects.all()
     template_name = 'cms_association/association_map_results.html'
+
+
+class AssociationView(DetailView):
+    template_name = 'cms_association/association.html'
+    model = Association
+    slug_url_kwarg = 'association'
+
+    def get_object(self, queryset=None):
+        if queryset is None:
+            queryset = self.get_queryset()
+        return get_object_or_404(queryset, slug=self.kwargs.get(self.slug_url_kwarg))
+
